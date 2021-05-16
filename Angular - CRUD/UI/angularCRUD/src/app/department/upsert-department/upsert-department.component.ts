8b99f824c56fe1d667b1898d926362908de56c54
Tareq Haider <muhammad.tareq.haider@gmail.com>
2021-05-16 21:51:08 +0600

    UI Init

---
+++ Angular - CRUD/UI/angularCRUD/src/app/department/upsert-department/upsert-department.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upsert-department',
+  templateUrl: './upsert-department.component.html',
+  styleUrls: ['./upsert-department.component.css']
+})
+export class UpsertDepartmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Angular - CRUD/UI/angularCRUD/src/app/employee/upsert-employee/upsert-employee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upsert-employee',
+  templateUrl: './upsert-employee.component.html',
+  styleUrls: ['./upsert-employee.component.css']
+})
+export class UpsertEmployeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
